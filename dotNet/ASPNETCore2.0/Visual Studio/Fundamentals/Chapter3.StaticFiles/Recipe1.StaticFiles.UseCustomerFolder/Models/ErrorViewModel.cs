e8809c92c35679a78d5f42ad37ac923d5c3ab9bc
sunwei <sunwei@gznb.com>
2017-08-31 16:56:32 +0800

    update

---
+++ dotNet/ASPNETCore2.0/Visual Studio/Fundamentals/Chapter3.StaticFiles/Recipe1.StaticFiles.UseCustomerFolder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recipe1.StaticFiles.UseCustomerFolder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ dotNet/ASPNETCore2.0/Visual Studio/Fundamentals/Chapter3.StaticFiles/Recipe1.StaticFiles.UseCustomerFolder/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace Recipe1.StaticFiles.UseCustomerFolder
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //启用默认的静态文件（wwwroot目录）访问
+            app.UseStaticFiles();
+
+            /*
+             * 自定义配置可访问的静态资源
+             * Example:http://localhost:54533/staticfiles/test.jpg
+             */
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                //配置MyStaticFiles目录可访问
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"MyStaticFiles")),
+                //设置访问路径
+                RequestPath = new PathString("/StaticFiles")
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
