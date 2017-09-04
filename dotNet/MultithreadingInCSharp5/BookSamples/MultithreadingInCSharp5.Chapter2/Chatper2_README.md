f757b99179799c428368d95caf51aa1230bb9345
sunwei <sunwei@gznb.com>
2017-09-04 17:43:06 +0800

    update

--- dotNet/MultithreadingInCSharp5/BookSamples/MultithreadingInCSharp5.Chapter2/Chatper2_README.md
+++ dotNet/MultithreadingInCSharp5/BookSamples/MultithreadingInCSharp5.Chapter2/Chatper2_README.md
@@ -1,4 +1,6 @@
-﻿### **线程同步**
+﻿### **Chapter2：线程同步**
 * Recipe1：执行基于原子的操作
 * Recipe2：使用Mutex类
 * Recipe3：使用SemaphoreSlim类
+  * SemaphoreSlim类：限制资源的并发数量。
+* Recipe4：使用AutoResetEvent类
---
+++ dotNet/MultithreadingInCSharp5/BookSamples/MultithreadingInCSharp5.Chapter2/Recipe3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+namespace Recipe3
+{
+    class Program
+    {
+        private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(4);
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                var threadName = $"thread{i}";
+                var secondsToWait = 2 + 2 * i;
+                var t = new Thread(()=>AccessDatabase(threadName, secondsToWait));
+                t.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void AccessDatabase(string name, int seconds)
+        {
+            Console.WriteLine($"{name} waits to access a database");
+            Semaphore.Wait();
+            Console.WriteLine($"{name} was granted an access to a database");
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine($"{name} is completed");
+            Semaphore.Release();
+        }
+    }
+}
